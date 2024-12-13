7a5ab80d5f7397fd623c9cd05d0c207aa939f709
Elizabeth <snowtoucan@outlook.com>
2024-12-13 19:17:35 +0000

    a

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 import * as dat from "lil-gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
