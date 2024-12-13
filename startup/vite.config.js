4480554931c64a63444169e4c61808d91f6b4ee4
Elizabeth <snowtoucan@outlook.com>
2024-12-13 18:51:47 +0000

    fix gh structure i think

--- startup/vite.config.js
+++ startup/vite.config.js
@@ -1,18 +1,19 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import vue from '@vitejs/plugin-vue';
+
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
 
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
-    server:
-    {
+    base: '/pins/',
+    plugins: [vue()], // Using the Vue plugin now
+    server: {
         host: true,
         open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
     }
-}
+};
